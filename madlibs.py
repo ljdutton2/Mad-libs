6c0562306043dcaf053f736d42ad0a30745303fc
lauren <ljdutton2@icloud.com>
2019-09-02 20:10:31 -0700

    handled improper user input

--- madlibs.py
+++ madlibs.py
@@ -10,20 +10,25 @@ get_title(name)
 
 
 
+def user_input(prompt):
+    user_input = input(prompt)
+    if user_input == "":
+        print ("please type a word")
+    return user_input
 
-user_input=input()
-adjective = input("Type an adjective")
-noun = input("Type a noun")
-verb_past = input("Type a past tense verb")
-adverb = input("Type an adverb")
-adjective_2 = input("Type another adjective")
-noun_2 = input("Type another noun")
-noun_3 = input("Type another noun")
-adjective_3 = input("Type another adjective")
-verb_2 = input("Type another verb")
-adverb_2 = input("Type another adverb")
-verb_past_2 = input("Type another past tense verb")
-adjective_4 = input("Type one last adjective")
+
+adjective = user_input("Type an adjective")
+noun = user_input("Type a noun")
+verb_past = user_input("Type a past tense verb")
+adverb = user_input("Type an adverb")
+adjective_2 = user_input("Type another adjective")
+noun_2 = user_input("Type another noun")
+noun_3 = user_input("Type another noun")
+adjective_3 = user_input("Type another adjective")
+verb_2 = user_input("Type another verb")
+adverb_2 = user_input("Type another adverb")
+verb_past_2 = user_input("Type another past tense verb")
+adjective_4 = user_input("Type one last adjective")
 
 words = [adjective,noun,verb_past,adverb,adjective_2,noun_2,noun_3,adjective_3,verb_2,adverb_2,verb_past_2,adjective_4]
 
@@ -36,6 +41,7 @@ print("""Today I went to the zoo. I saw a {} {} jumping up and down in its tree.
     When I got home I {} my mom for a {} day at the zoo. """.format(words[0],
     words[1], words[2], words[3], words[4], words[5], words[6], words[7], words[8],
     words[9], words[10], words[11]))
+
 
 
 
